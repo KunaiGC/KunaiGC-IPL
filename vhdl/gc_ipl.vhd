701b2e78ed1ccf42b8e3def1c7973f914af50d85
ManCloud <ch_ruecker@t-online.de>
2023-05-15 12:46:28 +0200

    updated for KunaiGC

--- vhdl/gc_ipl.vhd
+++ vhdl/gc_ipl.vhd
@@ -7,15 +7,17 @@ use ieee.std_logic_1164.all;
 
 entity gc_ipl is
     port (
-        exi_miso : out std_logic_vector(3 downto 0);
+        exi_miso : out std_logic_vector(1 downto 0);
         exi_mosi : in std_logic;
         exi_cs : in std_logic;
         exi_clk : in std_logic;
 
+		  led_pth : out std_logic;
+		  led_dis : out std_logic;
+
         f_miso : in std_logic;
         f_mosi : out std_logic;
-        f_cs : out std_logic;
-        f_clk : out std_logic
+        f_cs : out std_logic
     );
 end gc_ipl;
 
@@ -26,38 +28,40 @@ architecture Behavioral of gc_ipl is
         passthrough,
         ignore_wait,
         ignore,
+		  qoob_wait,
         disable
     );
     signal state : state_t;
-
+	 signal old_state : state_t;
+	 
     signal outbuf : std_logic_vector(5 downto 0);
     signal zero : std_logic;
-    signal one : std_logic;
-    signal bits : integer range 0 to 31;
+    signal bits : integer range 0 to 40;
 begin
-    f_clk <= 'Z'; -- This pin needs to go, clock should be routed directly on the PCB
     f_cs <= exi_cs when (state = translate or state = passthrough) else '1';
     f_mosi <= outbuf(5) when state = translate else exi_mosi;
-    exi_miso <= (others => f_miso) when (state /= ignore and state /= disable and exi_cs = '0') else (others => 'Z');
+    exi_miso <= (others => f_miso) when (state /= ignore and state /= disable and state /= qoob_wait and exi_cs = '0') else (others => 'Z');
+
+	 led_pth <= '1' when state = passthrough else '0';
+	 led_dis <= '1' when state = disable else '0';
 
     process (exi_cs, exi_clk, state)
     begin
         if exi_cs = '1' then
-            if state /= disable then
+            if (state /= disable) then
                 state <= translate;
             end if;
             outbuf <= (others => '0');
             zero <= '0';
-            one <= '1';
             bits <= 0;
         elsif rising_edge(exi_clk) then
+				if bits < 40 then
+					bits <= (bits + 1);
+				end if;
+				zero <= zero or exi_mosi;
             case state is
                 when translate | passthrough_wait | ignore_wait =>
                     outbuf <= outbuf(4 downto 0) & exi_mosi;
-                    zero <= zero or exi_mosi;
-                    one <= one and exi_mosi;
-                    bits <= bits + 1;
-
                     case bits is
                         when 0 =>
                             outbuf(0) <= '1';
@@ -68,18 +72,23 @@ begin
                             end if;
 
                         when 1 =>
-                            outbuf(0) <= '1';
-
-                        when 2 | 3 | 4 | 5 | 6 =>
-                            -- Flash is only 512KB, no point in overriding accesses higher than that
-                            -- Let it all fall through to the IPL ROM (for fonts) and other peripherals
+									-- When getting 0xcXXXXXXX await disable 
+									 if exi_mosi = '1' then
+										old_state <= ignore;
+										state <= qoob_wait;
+										zero <= '0';
+									 else
+										outbuf(0) <= '1';
+									 end if;
+                        when 2 | 3 =>
+                            -- Flash is 2048KB, no point in overriding accesses higher than that
                             if exi_mosi = '1' then
                                 state <= ignore_wait;
                             end if;
 
                         when 15 =>
                             -- Ignore reads below 0x800, we don't provide a BS1
-                            if zero = '0' then
+                            if (zero = '0') then
                                 state <= ignore;
                             end if;
 
@@ -87,22 +96,52 @@ begin
                             if state = ignore_wait then
                                 state <= ignore;
                             end if;
-
-                            if one = '1' then
-                                state <= disable;
-                            end if;
-
+									 
                         when 31 =>
-                            state <= passthrough;
-
+									state <= passthrough;
                         when others =>
-                            null;
+									null;
                     end case;
-
+					when qoob_wait =>				
+						case bits is 
+							when 37 | 38 =>
+								if (exi_mosi /= '1') and (old_state = ignore) then
+									state <= old_state;
+								elsif (exi_mosi /= '0') and (old_state = disable) then
+									state <= old_state;
+								end if;
+							when 39 => 
+								if (exi_mosi = '0') and (old_state = ignore) then
+									state <= disable;
+								elsif (exi_mosi = '1') and (old_state = disable) then
+									state <= ignore;
+								end if;
+							when 40 =>
+								state <= old_state;
+							when others =>
+								if zero /= '0' then
+									state <= old_state;
+								end if;
+						end case;
                 when passthrough =>
                     null;
-
-                when ignore | disable =>
+					 when ignore =>
+						null;
+                when disable =>
+						case bits is
+							when 0 => 
+								if exi_mosi /= '1' then 
+									bits <= 2;	-- skip execution of 'when 1'
+								end if;
+							when 1 =>
+								if exi_mosi = '1' then
+									zero <= '0';
+									old_state <= disable;
+									state <= qoob_wait;
+								end if;
+							when others =>
+								null;
+						end case;
                     null;
             end case;
         end if;
